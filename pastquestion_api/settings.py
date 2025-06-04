15919831be7132c96a24af093924ee13c4954536
nwakalordivine <johndoe@example.com>
2025-06-04 16:44:15 +0100

    update settings

--- pastquestion_api/settings.py
+++ pastquestion_api/settings.py
@@ -107,10 +107,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'postgres.mpwdwjezdiqkyaaebewz',
         'PASSWORD': 'pastquestion_backend',
-        'HOST': 'db.mpwdwjezdiqkyaaebewz.supabase.co',
-        'PORT': '5432',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
         'OPTIONS': {
             'sslmode': 'require',
         },
